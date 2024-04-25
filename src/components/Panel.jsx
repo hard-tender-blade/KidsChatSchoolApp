b10f394827128bd5aa2d0c308a6c02f2c32b8907
hardtenderblade <matsenkodenis0@gmail.com>
2024-04-25 16:17:55 +0200

    Panel update

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,14 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Panel() {
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSend = () => {
+    console.log(name, message);
+  };
+
   return (
     <div className="w-full h-full flex ">
       <div className="w-10/12 bg-red-300 flex flex-col">
-        <input type="text" placeholder="name" className="p-4 text-2xl" />
-        <input type="text" placeholder="message" className="p-4 text-2xl" />
+        <input
+          type="text"
+          placeholder="name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          className="p-4 text-2xl"
+        />
+        <input
+          type="text"
+          placeholder="message"
+          value={message}
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          className="p-4 text-2xl"
+        />
       </div>
 
-      <button className="w-2/12 m-10 bg-blue-400">Send</button>
+      <button className="w-2/12 m-10 bg-blue-400" onClick={handleSend}>
+        Send
+      </button>
     </div>
   );
 }
