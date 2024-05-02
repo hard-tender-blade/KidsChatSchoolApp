fa7ed1551992131e83a0988c6c32cff80efc5751
hardtenderblade <matsenkodenis0@gmail.com>
2024-05-02 16:27:59 +0200

    Firebase conf

---
+++ src/api/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDfFIb7Z6k6pQt5QsmIdQ2AgV1Jx8XJ2P0",
+  authDomain: "schoolchatappkids.firebaseapp.com",
+  projectId: "schoolchatappkids",
+  storageBucket: "schoolchatappkids.appspot.com",
+  messagingSenderId: "190568430302",
+  appId: "1:190568430302:web:8af203db979e17d2b0204f",
+};
+
+const app = initializeApp(firebaseConfig);
