1433ccf0cd2e57410c2d2cd1d037e463ce923b89
hardtenderblade <matsenkodenis0@gmail.com>
2024-05-09 17:10:09 +0200

    Google

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -4,8 +4,10 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 export default function LoginScreen() {
+  const provider = new GoogleAuthProvider();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -28,7 +30,7 @@ export default function LoginScreen() {
     setPassword("");
   };
 
-  const handleLogin = () => {
+  const handleLogin = async () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed up
@@ -45,6 +47,30 @@ export default function LoginScreen() {
 
     setEmail("");
     setPassword("");
+  };
+
+  const handleGoogle = async () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.error(errorCode, errorMessage, email, credential);
+      });
   };
 
   return (
@@ -73,6 +99,9 @@ export default function LoginScreen() {
       </button>
       <button onClick={handleLogin} className="p-4 bg-blue-400 text-2xl">
         Login
+      </button>
+      <button onClick={handleGoogle} className="p-4 bg-blue-400 text-2xl">
+        Google
       </button>
     </div>
   );
