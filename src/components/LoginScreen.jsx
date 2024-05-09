c81880051f6204fd5ab1069fdafa329b94050555
hardtenderblade <matsenkodenis0@gmail.com>
2024-05-09 16:48:50 +0200

    Implemented login

--- src/components/LoginScreen.jsx
+++ src/components/LoginScreen.jsx
@@ -1,13 +1,35 @@
 import React, { useState } from "react";
 import { auth } from "../api/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 export default function LoginScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleLogin = async () => {
+  const handleCreate = async () => {
     createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage);
+        // ..
+      });
+
+    setEmail("");
+    setPassword("");
+  };
+
+  const handleLogin = () => {
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed up
         const user = userCredential.user;
@@ -46,6 +68,9 @@ export default function LoginScreen() {
         className="p-4 text-2xl"
       />
 
+      <button onClick={handleCreate} className="p-4 bg-blue-400 text-2xl">
+        Create acc
+      </button>
       <button onClick={handleLogin} className="p-4 bg-blue-400 text-2xl">
         Login
       </button>
